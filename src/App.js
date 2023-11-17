e2f9647f513fd380ce98dd50282d6361658b175e
Lipsamayee Jena <lipsamayeejena760@gmail.com>
2023-11-17 10:26:18 +0530

    geek foods

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import Navbar from './Component/Navbar';
+import Main from './Component/Main';
+import Section2 from './Component/Section2';
+import Section3 from './Component/Section3';
+import Footer from './Component/Footer';
+
+// import QuotecardList from './Component/QuotecardList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+      <Section2 />
+      <Section3 />
+      <Footer />
+      {/* <QuotecardList /> */}
+
+    </div>
+  );
+}
+
+export default App;
